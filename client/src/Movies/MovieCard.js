30c0d531ff22809ca7d9b41c8f2c19ba12a0daa2
Stephen Gary <stgary0@gmail.com>
2020-02-25 18:29:21 -0500

    routes

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const MovieCard = props => {
-  return;
-};
+  return (
+    <div className="movie-card">
+      <Link to = {`/movies/${props.id}`}>
+    <h2>{props.title}</h2>
+    </Link>
+    <div className="movie-director">
+      Director: <em>{props.director}</em>
+    </div>
+    <div className="movie-metascore">
+      Metascore: <strong>{props.metascore}</strong>
+    </div>
+    <h3>Actors</h3>
 
+    {props.stars.map(star => (
+      <div key={star} className="movie-star">
+        {star}
+      </div>
+    ))}
+  </div>
+  );
+}
 export default MovieCard;
