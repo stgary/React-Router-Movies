4cd6266e073e74e65f4591914e12235edc80ef09
Stephen Gary <stgary0@gmail.com>
2020-02-25 18:39:59 -0500

    Home button

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+
+  const history = useHistory();
+  const routeToHome = () => {
+    history.push("/");
+  };
+
+  return(
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div onClick={routeToHome} className="home-button">Home</div>
+    </div>
+  );
+}
 
 export default SavedList;
